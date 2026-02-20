8d9b189c75e28abb6e365a928db724bb678797ae
LittleNurgling <56669654+LittleNurgling@users.noreply.github.com>
2026-02-20 07:44:55 +0300

    Create index_scripts.js

---
+++ index_scripts.js
@@ -0,0 +1,11 @@
+document.getElementById("goto-dataslate").addEventListener("click", () => {
+  window.location.href = "./character_dataslate.html"; // путь к проекту
+});
+
+document.getElementById("goto-project2").addEventListener("click", () => {
+  alert("Проект 2 пока не готов");
+});
+
+document.getElementById("goto-project3").addEventListener("click", () => {
+  alert("Проект 3 пока не готов");
+});
